ccbbcf1842097a35a18adda21c64279b3fd62c05
yousef-kazim-student-odisee <32954570+yousef-kazim-student-odisee@users.noreply.github.com>
2018-03-21 10:17:51 +0100

    Add files via upload
    bij het reken machine optellen werkt niet, de getallen worden naast elkaar gezet

---
+++ Les 1/Opdracht.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var getal1 = document.getElementById("getal1").value;
+var getal2 = document.getElementById("getal2").value;
+
+
+function sub() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    document.getElementById("uitkomst").value = getal1 - getal2;
+
+}
+
+function multiply() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    document.getElementById("uitkomst").value = getal1 * getal2;
+}
+
+function divide() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    document.getElementById("uitkomst").value = getal1 / getal2;
+}
+
+
+function sum() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    document.getElementById("uitkomst").value = getal1 + getal2;
+}
